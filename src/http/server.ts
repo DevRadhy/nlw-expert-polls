3a6e5155364ebf886a1dd5f984e6344533e9cd6f
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2024-02-08 13:47:46 -0300

    Aula 03

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,9 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
 
@@ -10,8 +12,12 @@ app.register(cookie, {
   hook: 'onRequest',
 })
 
+app.register(websocket)
+
 app.register(createPoll)
 app.register(getPoll)
+
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
